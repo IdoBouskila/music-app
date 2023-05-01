49ab259f9741871cf027e22274ccb9f9629f7e1e
Ido Bouskila <idobouskila22@gmail.com>
2023-05-01 12:22:21 +0300

    refactor: wrap app layout with provider for global context access

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { SongProvider } from "./context/SongProvider";
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{ children }</body>
+      <body>
+        <SongProvider>
+          { children }
+        </SongProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,12 @@
 import Player from "./components/Player";
-import { SongProvider } from "./context/SongProvider";
 import TrendingSongs from "./trending_songs/page";
 
 export default function Home() {
   return (
-    <SongProvider>
       <div>
         <h1>Home</h1>
           <Player />
           <TrendingSongs />
       </div>
-    </SongProvider>
   )
 }
