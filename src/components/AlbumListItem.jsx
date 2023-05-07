f519d51267de1262c2f579a629d8659ea92d18d5
Ido Bouskila <idobouskila22@gmail.com>
2023-05-07 23:45:06 +0300

    style: format spaces

--- src/components/AlbumListItem.jsx
+++ src/components/AlbumListItem.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const AlbumListItem = ({ id, title, imgSrc, releaseDate }) => {
     return (
-        <Link href={`album/${ id }`}>
+        <Link href={ `album/${ id }` }>
             <div>
                 <img src={ imgSrc } alt="" />
                 <span>{ title }</span>
