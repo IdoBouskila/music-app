c775df0991af902b3a97f17b1f6e20768271bfa0
Ido Bouskila <idobouskila22@gmail.com>
2023-05-14 11:22:56 +0300

    refactor: update class names and design side navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,39 +1,67 @@
-import { AiFillHome } from 'react-icons/ai';
-import { FiMusic, FiUsers, FiRadio } from 'react-icons/fi';
+import { AiFillHome, AiFillGithub } from 'react-icons/ai';
+import { FiMusic, FiUsers, FiRadio, FiHeart } from 'react-icons/fi';
 import { GiMusicSpell } from 'react-icons/gi';
 import NavLink from './NavLink';
+import Link from 'next/link';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
-            <GiMusicSpell className='app-logo'/>
+            <Link className='app-logo' href='/' >
+                <GiMusicSpell />
+                <span>Music App</span>
+            </Link>
 
-            <ul>
-                <li>
-                    <NavLink slug=''>
-                        <AiFillHome />
-                    </NavLink>
-                </li>
+            <div>
+                <h2>DISCOVER</h2>
 
-                <li>
-                    <NavLink slug='trending_songs'>
-                        <FiMusic />
-                    </NavLink>
-                </li>
+                <ul>
+                    <li>
+                        <NavLink slug=''>
+                            <AiFillHome />
+                            <span>Home</span>
+                        </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink slug='top_artists'>
-                        <FiUsers />
-                    </NavLink>
-                </li>
+                    <li>
+                        <NavLink slug='trending_songs'>
+                            <FiMusic />
+                            <span>Songs</span>
+                        </NavLink>
+                    </li>
 
-                <li>
-                    <NavLink slug='radio'>
-                        <FiRadio />
-                    </NavLink>
-                </li>
+                    <li>
+                        <NavLink slug='top_artists'>
+                            <FiUsers />
+                            <span>Artists</span>
+                        </NavLink>
+                    </li>
 
-            </ul>
+                    <li>
+                        <NavLink slug='radio'>
+                            <FiRadio />
+                            <span>Radio</span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h2>LIBRARY</h2>
+                <ul>
+                    <li>
+                        <NavLink slug='favorites'>
+                            <FiHeart />
+                            <span>Favorites</span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <a href='https://github.com/IdoBouskila/' target='_blank'>
+                <AiFillGithub />
+                <span>Github</span>
+            </a>
         </div>
     );
 };
