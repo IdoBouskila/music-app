7297441dc814518c80e62c006e132c1bc67f4a2b
Ido Bouskila <idobouskila22@gmail.com>
2023-06-09 17:30:39 +0300

    refactor: remove unused audio html tag

--- src/context/SongProvider.jsx
+++ src/context/SongProvider.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { createContext, useContext, useRef, useState } from "react"
+import { createContext, useContext, useState } from "react"
 
 const SongContext = createContext();
 
@@ -8,12 +8,10 @@ export const useSong = () => {
 }
 
 export const SongProvider = ({ children }) => {
-    const audioRef = useRef(null);
     const [playlist, setPlaylist] = useState([]);
     const [currentId, setCurrentId] = useState(null);
 
     const value = {
-        audioRef,
         currentSong: playlist?.[currentId] || {},
         handleSongClick: (songIndex, targetPlaylist) => {
             setCurrentId(songIndex);
@@ -26,25 +24,17 @@ export const SongProvider = ({ children }) => {
         
             setCurrentId((prev) => prev - 1);
         },
-        handleNextSong,
-    }
-
-    function handleNextSong() {
-        if(playlist.length - 1 <= currentId) {
-            return setCurrentId(0);
+        handleNextSong: () => {
+            if(playlist.length - 1 <= currentId) {
+                return setCurrentId(0);
+            }
+            
+            setCurrentId((prev) => prev + 1);
         }
-        
-        setCurrentId((prev) => prev + 1);
     }
 
     return (
         <SongContext.Provider value={value}>
-            <audio
-                autoPlay
-                onEnded={ handleNextSong }
-                src={ playlist?.[currentId]?.preview }
-                ref={ audioRef }
-            />
             { children }
         </SongContext.Provider>
     );
