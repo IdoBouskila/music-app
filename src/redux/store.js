195cef0fa21df47ccc8642ce4619b7ce12960b49
Ido Bouskila <idobouskila22@gmail.com>
2023-07-20 00:11:12 +0300

    chore: ignore data serializable check in `Redux Persist`

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import { configureStore } from "@reduxjs/toolkit";
 import songsReducer from './features/songsSlice';
 import favoritesReducer from "./features/favoritesSlice";
-import persistReducer from "redux-persist/es/persistReducer";
 import storage from './utils/storage';
 
 const persistConfig = {
@@ -15,7 +15,13 @@ export const store = configureStore({
     reducer: {
         songs: songsReducer,
         favorites: persistedReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
 export default store;
